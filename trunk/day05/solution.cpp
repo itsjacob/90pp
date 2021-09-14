6429338dfd45fa0d23ea453d699f62aa90807ca4
itsjacob <itsjacobking@gmail.com>
2021-09-13 22:25:28 -0500

    refactor: amortized O(1) time complexity of pop/peak solution to Leetcode #232

--- trunk/day05/solution.cpp
+++ trunk/day05/solution.cpp
@@ -10,40 +10,37 @@ public:
   /** Removes the element from in front of queue and returns that element. */
   int pop()
   {
-    while (!mystack.empty()) {
-      mystack_helper.push(mystack.top());
-      mystack.pop();
+    // Always get the item from cache
+    // if we cache is empty, we dump the items from mystack in reverse order
+    if (mystack_cache.empty()) {
+      while (!mystack.empty()) {
+        mystack_cache.push(mystack.top());
+        mystack.pop();
+      }
     }
-    int res = mystack_helper.top();
-    mystack_helper.pop();
-    while (!mystack_helper.empty()) {
-      mystack.push(mystack_helper.top());
-      mystack_helper.pop();
-    }
+    int res = mystack_cache.top();
+    mystack_cache.pop();
     return res;
   }
 
   /** Get the front element. */
   int peek()
   {
-    while (!mystack.empty()) {
-      mystack_helper.push(mystack.top());
-      mystack.pop();
+    if (mystack_cache.empty()) {
+      while (!mystack.empty()) {
+        mystack_cache.push(mystack.top());
+        mystack.pop();
+      }
     }
-    int res = mystack_helper.top();
-    while (!mystack_helper.empty()) {
-      mystack.push(mystack_helper.top());
-      mystack_helper.pop();
-    }
-    return res;
+    return mystack_cache.top();
   }
 
   /** Returns whether the queue is empty. */
-  bool empty() { return mystack.empty(); }
+  bool empty() { return mystack.empty() && mystack_cache.empty(); }
 
 private:
   std::stack<int> mystack;
-  std::stack<int> mystack_helper;
+  std::stack<int> mystack_cache;
 };
 
 /**
