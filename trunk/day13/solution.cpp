3de6bfbd756f5210d4a658a922d8bcc0645282f1
itsjacob <itsjacobking@gmail.com>
2021-09-21 21:06:35 -0500

    feat: O(n) time complexity O(logN) space complexity iterative dfs solution to Leetcode #104

--- trunk/day13/solution.cpp
+++ trunk/day13/solution.cpp
@@ -10,11 +10,36 @@
  * };
  */
 
-#define maxDepth maxDepthRecursive
+//#define maxDepth maxDepthRecursive
+#define maxDepth maxDepthIterative_dfs_v1
+//#define maxDepth maxDepthIterative_dfs_v2
+//#define maxDepth maxDepthIterative_bfs
 
 class Solution
 {
 public:
+  int maxDepthIterative_dfs_v1(TreeNode *root)
+  {
+    if (root == nullptr) return 0;
+    // dfs approach
+    std::stack<std::pair<TreeNode *, int>> theStack;
+    theStack.emplace(root, 1);
+    int maxDepth{ 0 };
+    while (!theStack.empty()) {
+      TreeNode *subroot = theStack.top().first;
+      int curDepth = theStack.top().second;
+      maxDepth = std::max(maxDepth, curDepth);
+      theStack.pop();
+      if (subroot->left != nullptr) {
+        theStack.emplace(subroot->left, curDepth + 1);
+      }
+      if (subroot->right != nullptr) {
+        theStack.emplace(subroot->right, curDepth + 1);
+      }
+    }
+    return maxDepth;
+  }
+
   int maxDepthRecursive(TreeNode *root)
   {
     if (root == nullptr) return 0;
